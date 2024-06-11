36be89feb3b54ed7bf60723dcfece53b1283ab2d
Josh McAlpin <josh.mcalpin@webcemeteries.com>
2024-06-11 08:26:48 -0500

    target file root

--- ImagingAdjusters/src/HEIC_to_JPG.py
+++ ImagingAdjusters/src/HEIC_to_JPG.py
@@ -4,9 +4,10 @@ target_folder = input('Enter the target file directory:')
 
 for file in os.listdir(target_folder):
     if file.upper().endswith('.HEIC'):
+        root = os.path.abspath(file)
         new_name = f'{file.split('.')[0]}.jpg'
         new_path = os.path.join(root, new_name)
         old_path = os.path.join(root, file)
         os.rename(old_path, new_path)
-        
+
 input('Finished converting HEIC files.  Press any key to continue.')
